c6b8a92e2cf7426c205eb065ae84cba4df16c47e
Itsmenabin <jenexthegreat@gmail.com>
2020-07-25 18:25:08 +0545

    Movement

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,31 +4,55 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
     private void ProcessInput()
     {
+        
+        
+    }
+    private void Thrust()
+    {
         if(Input.GetKey(KeyCode.Space))
         {
-
+            rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else
+                {
+                audioSource.Stop();
+            }
         }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation= true; //take manual control of rotation
         if(Input.GetKey(KeyCode.D))
         {
-
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            
+            transform.Rotate(-Vector3.forward);
         }
+        rigidBody.freezeRotation = false; //take physical control of rotation
     }
+    
 }
